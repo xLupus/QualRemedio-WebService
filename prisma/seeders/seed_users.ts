5c4d8289ec00c9f68f315f1564ceb74e5c93dea1
Mr.R4F <rafael.soares0777@gmail.com>
2023-11-03 14:36:31 -0300

    correção na seeder de user

--- prisma/seeders/seed_users.ts
+++ prisma/seeders/seed_users.ts
@@ -8,35 +8,54 @@ export async function users(): Promise<void> {
     const passwordHash: string = await bcrypt.hash('12345678', 15);
     const date: string = new Date().toISOString();
 
-    await prisma.user.createMany({
-        data: [
-            {
+    const roleAdmin: number = 5;
+    const rolePatitent: number = 1;
+
+    await prisma.$transaction([
+        prisma.user.create({
+            data: {
                 name: "Mr.Raf",
                 email: "mrraf@admin.com",
                 password: passwordHash,
                 cpf: "12345678901",
                 birth_day: date,
                 telephone: "12345678900",
-                role_id: 5
-            },
-            {
+                role: {
+                    connect: {
+                        id: roleAdmin
+                    }
+                }
+            }
+        }),
+        prisma.user.create({
+            data: {
                 name: "Lupus",
                 email: "lupus@admin.com",
                 password: passwordHash,
                 cpf: "12345678903",
                 birth_day: date,
                 telephone: "12345678902",
-                role_id: 5
-            },
-            {
+                role: {
+                    connect: {
+                        id: roleAdmin
+                    }
+                }
+            }
+        }),
+        prisma.user.create({
+            data: {
                 name: "Test",
                 email: "test@test.com",
                 password: passwordHash,
                 cpf: "12345678904",
                 birth_day: date,
                 telephone: "12345678905",
-                role_id: 1
+                role: {
+                    connect: {
+                        id: rolePatitent
+                    }
+                }
             }
-        ]
-    });
+        })
+    ]);
 }
